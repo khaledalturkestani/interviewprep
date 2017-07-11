facff5659a18b85602e1b4501f882f9a1057c00d
Khaled AlTurkestani <khaled@ooma.com>
2017-07-11 11:12:16 -0700

    QuickSort and MergeSort

---
+++ src/main/java/MergeSort.java
@@ -0,0 +1,88 @@
+public class MergeSort {
+
+    public static void sort(int[] arr) {
+        if (arr == null)
+            return;
+
+        sortUtil(arr, 0, arr.length);
+    }
+
+    public static void sortUtil(int[] arr, int start, int len) {
+        if (len <= 1) return;
+
+        int mid = len/2;
+        // Sort left half
+        sortUtil(arr, start, mid);
+        // Sort right half
+        sortUtil(arr, start+mid, len-mid);
+
+        // Make a copy of left half
+        int[] left = new int[mid];
+        for (int i = 0; i < left.length; i++) {
+            left[i] = arr[start+i];
+        }
+        // Make a copy of right half
+        int[] right = new int[len-mid];
+        for (int j = 0; j < right.length; j++) {
+            right[j] = arr[start+mid+j];
+        }
+
+        // Merge the two halves
+        int lIdx = 0;
+        int rIdx = 0;
+        int idx = start;
+        while (lIdx < left.length) {
+            while (rIdx < right.length && right[rIdx] <= left[lIdx]) {
+                arr[idx] = right[rIdx];
+                idx++;
+                rIdx++;
+            }
+            arr[idx] = left[lIdx];
+            idx++;
+            lIdx++;
+        }
+        while (rIdx < right.length) {
+            arr[idx] = right[rIdx];
+            idx++;
+            rIdx++;
+        }
+    }
+
+    private static void printArr(int[] arr) {
+        if (arr == null) return;
+
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i != arr.length-1)
+                System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1, 5, 2, 3, 3};
+        sort(arr);
+        printArr(arr);
+
+        int arr2[] = {1, 2, 1, 2, 1, 2, 1, 2};
+        sort(arr2);
+        printArr(arr2);
+
+        int arr3[] = {1, 1, 1, 1, 1, 1};
+        sort(arr3);
+        printArr(arr3);
+
+        int arr4[] = {2, 2, 2, 1, 1, 1};
+        sort(arr4);
+        printArr(arr4);
+
+        int arr5[] = {2};
+        sort(arr5);
+        printArr(arr5);
+
+        int arr6[] = {};
+        sort(arr6);
+        printArr(arr6);
+    }
+}
---
+++ src/main/java/QuickSort.java
@@ -0,0 +1,73 @@
+/**
+ * Created by Khaled.AlTurkestani on 7/7/2017.
+ */
+public class QuickSort {
+
+    public static void sort(int[] arr) {
+        if (arr == null)
+            return;
+
+        sortUtil(arr, 0, arr.length-1);
+    }
+
+    private static void sortUtil(int arr[], int left, int right) {
+        if (right - left <= 1)
+            return;
+
+        int pivot = right;
+        int low = left;
+        int high = right-1;
+        while (low <= high) {
+            if (arr[low] <= arr[pivot]) {
+                low++;
+            } else if (arr[high] > arr[pivot]) {
+                high--;
+            } else {
+                int temp = arr[low];
+                arr[low] = arr[high];
+                arr[high] = temp;
+                low++;
+                high--;
+            }
+        }
+        // Switch with 'low'
+        int temp = arr[low];
+        arr[low] = arr[pivot];
+        arr[pivot] = temp;
+        pivot = low;
+
+        sortUtil(arr, left, pivot-1);
+        sortUtil(arr, pivot+1, right);
+    }
+
+    private static void printArr(int[] arr) {
+        if (arr == null)
+            return;
+
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i != arr.length-1)
+                System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1, 5, 2, 3, 3};
+        sort(arr);
+        printArr(arr);
+
+        int arr2[] = {1, 2, 1, 2, 1, 2, 1, 2};
+        sort(arr2);
+        printArr(arr2);
+
+        int arr3[] = {1, 1, 1, 1, 1, 1};
+        sort(arr3);
+        printArr(arr3);
+
+        int arr4[] = {2, 2, 2, 1, 1, 1};
+        sort(arr4);
+        printArr(arr4);
+    }
+}
