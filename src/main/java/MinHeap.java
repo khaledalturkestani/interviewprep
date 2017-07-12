a98f529a8ff09e7756b5efa911e785d598aa8d97
Khaled AlTurkestani <khaledalturkestani@Khaleds-MacBook-Pro.local>
2017-07-12 13:02:49 -0700

    MinHeap - completed

--- src/main/java/MinHeap.java
+++ src/main/java/MinHeap.java
@@ -34,25 +34,81 @@ public class MinHeap {
         return min;
     }
 
-    public void decreaseKey(int key, int new_val) {
+    public void decreaseKey(int indx, int new_val) {
+        if (indx >= heapSize)
+            throw new ArrayIndexOutOfBoundsException(indx);
 
+        if (arr[indx] < new_val)
+            throw new RuntimeException(new_val + " is greater than current value " + arr[indx]);
+
+        arr[indx] = new_val;
+        while (indx >= 0) {
+            int parent = parent(indx);
+            if (arr[parent] < arr[indx]) {
+                break;
+            }
+            swap(parent, indx);
+        }
     }
 
     public void deleteKey(int indx) {
-        if (indx >= heapSize)
-            return;
-
-        arr[indx] = arr[heapSize-1];
-        heapSize--;
-        heapify(indx);
+        decreaseKey(indx, Integer.MIN_VALUE);
+        extractMin();
     }
 
-    public void insertKey(int key) {
+    /**
+     * Returns index of key in the MinHeap array.
+     *
+     * @param key
+     * @return
+     */
+    public int insertKey(int key) {
+        if (heapSize == arr.length)
+            return -1;
 
+        heapSize++;
+        int indx = heapSize - 1;
+        arr[indx] = key;
+
+        while (indx >= 0) {
+            int parent = parent(indx);
+            if (arr[parent] <= arr[indx]) {
+                break;
+            }
+            swap(indx, parent);
+            indx = parent;
+        }
+        return indx;
     }
 
     private void heapify(int indx) {
+        if (indx >= heapSize)
+            return;
 
+        int left = left(indx);
+        int right = right(indx);
+        int smaller = indx;
+        if (left < heapSize && arr[left] < arr[smaller])
+            smaller = left;
+        if (right < heapSize && arr[right] < arr[smaller])
+            smaller = right;
+        if (smaller != indx) {
+            swap(smaller, indx);
+            heapify(smaller);
+        }
+    }
+
+    public int size() {
+        return heapSize;
+    }
+
+    private void swap(int i1, int i2) {
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
+    }
+    private int parent(int indx) {
+        return (indx - 1) / 2;
     }
 
     private int left(int indx) {
@@ -61,5 +117,20 @@ public class MinHeap {
 
     private int right(int indx) {
         return indx * 2 + 2;
+    }
+
+    public static void main(String args[]) {
+        MinHeap h = new MinHeap(16);
+        h.insertKey(3);
+        h.insertKey(2);
+        h.deleteKey(1);
+        h.insertKey(15);
+        h.insertKey(5);
+        h.insertKey(4);
+        h.insertKey(45);
+        int heapSize = h.size();
+        for (int i = 0; i < heapSize; i++) {
+            System.out.println(h.extractMin());
+        }
     }
 }
