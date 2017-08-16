6ee9c708828c25522f6b6a9151b03526100a5b03
Khaled AlTurkestani <khaledalturkestani@Khaleds-MacBook-Pro.local>
2017-08-15 22:00:58 -0700

    Find element in array that appears only once (XOR implementation)

--- src/main/java/com/khaled/array/FindElementApprearsOnlyOnce.java
+++ src/main/java/com/khaled/array/FindElementApprearsOnlyOnce.java
@@ -38,19 +38,38 @@ public class FindElementApprearsOnlyOnce {
         return arr[i];
     }
 
+    public static int findElemXOR(int[] arr) {
+        int xor = 0;
+        for (int i = 0; i < arr.length; i++) {
+            xor = xor^arr[i];
+        }
+        return xor;
+    }
+
     public static void main(String args[]) {
         // Expected: 4
         int[] arr1 = {1, 1, 3, 3, 4, 5, 5, 7, 7, 8, 8};
         System.out.println(findElem(arr1));
 
-        // Expected: 1
-        int[] arr3 = {1, 3, 3, 4, 4, 5, 5, 7, 7, 8, 8};
-        System.out.println(findElem(arr3));
-
         // Expected: 8
         int[] arr2 = {1, 1, 3, 3, 4, 4, 5, 5, 7, 7, 8};
         System.out.println(findElem(arr2));
 
+        // Expected: 1
+        int[] arr3 = {1, 3, 3, 4, 4, 5, 5, 7, 7, 8, 8};
+        System.out.println(findElem(arr3));
+
+        // Expected: 4
+        int[] arr4 = {1, 1, 3, 3, 4, 5, 5, 7, 7, 8, 8};
+        System.out.println(findElemXOR(arr4));
+
+        // Expected: 8
+        int[] arr5 = {1, 1, 3, 3, 4, 4, 5, 5, 7, 7, 8};
+        System.out.println(findElemXOR(arr5));
+
+        // Expected: 1
+        int[] arr6 = {1, 3, 3, 4, 4, 5, 5, 7, 7, 8, 8};
+        System.out.println(findElemXOR(arr6));
     }
 }
 
