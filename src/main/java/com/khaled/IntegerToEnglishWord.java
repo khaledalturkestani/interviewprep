d22d3257ab4e9ab4dc61ee825e2d8c552745e915
Khaled AlTurkestani <khaledalturkestani@Khaleds-MacBook-Pro.local>
2017-11-30 16:31:09 -0800

    Integer to English word. Works.

--- src/main/java/com/khaled/IntegerToEnglishWord.java
+++ src/main/java/com/khaled/IntegerToEnglishWord.java
@@ -10,7 +10,7 @@ public class IntegerToEnglishWord {
         String str = Integer.toString(n);
         int[] arr = new int[str.length()];
         for (int i = 0; i < str.length(); i++) {
-            arr[i] = str.charAt(str.length()-i-1)-'0';
+            arr[i] = str.charAt(i)-'0';
         }
         return arr;
     }
@@ -20,56 +20,74 @@ public class IntegerToEnglishWord {
         String[] digits = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
         String[] teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
         String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-        int i = end -1;
-        int mag = begin / 3;
+        int len = end - begin;
+        int i = begin;
+        int mag = (arr.length-end) / 3;
         StringBuilder sb = new StringBuilder();
-        if (end-begin == 3) {
+        if (len == 3) {
             if (arr[i] != 0)
                 sb = sb.append(digits[arr[i]] + " Hundred ");
-            i--;
+            i++;
         }
-        if (arr[i] == 0) {
-            sb = sb.append(digits[arr[i-1]] + " ");
-        } else if (arr[i] == 1) {
-            sb = sb.append(teens[arr[i-1]] + " ");
-        } else if (arr[i] > 1) {
-            sb = sb.append(tens[arr[i--]] + " ");
-            if (arr[i] > 0) {
-                sb = sb.append(digits[arr[i]] + " ");
+        if (len >= 2) {
+            if (arr[i] == 0 && arr[i+1] != 0) {
+                sb = sb.append(digits[arr[i+1]] + " ");
+            } else if (arr[i] == 1) {
+                sb = sb.append(teens[arr[i+1]] + " ");
+            } else if (arr[i] > 1) {
+                sb = sb.append(tens[arr[i++]] + " ");
+                if (arr[i] > 0) {
+                    sb = sb.append(digits[arr[i]] + " ");
+                }
             }
         }
-        if (mag > 0)
+        if (len == 1) {
+            sb = sb.append(digits[arr[i]] + " ");
+        }
+        if (mag > 0 && sb.length() > 0)
             sb = sb.append(magnitudes[mag] + " ");
 
         return sb.toString();
     }
 
 
-    private static String intToEng(int n) {
+    private static String intToEng(int num) {
         // TODO - throw exception if negative
+        // Edge case:
+        if (num == 0)
+            return "Zero";
         // Convert to digits array
-        int[] arr = convertToArr(n);
-        int begin = arr.length - ((arr.length % 3 == 0) ? 3 : arr.length % 3);
-        int end = arr.length;
+        int[] arr = convertToArr(num);
+        int begin = 0;
+        int end = (arr.length % 3 == 0) ? 3 : arr.length % 3;
         StringBuilder sb = new StringBuilder();
-        while (begin >= 0) {
+        while (begin < arr.length) {
             String chunk = constructNumberWord(arr, begin, end);
             sb = sb.append(chunk);
-            end -= (end-begin);
-            begin -= 3;
+            begin = end;
+            end += 3;
         }
         return sb.toString().trim();
     }
 
     public static void main(String[] args) {
+//        String str = intToEng(1);
+//        System.out.println("Exp: One");
+//        System.out.println("Got: " + str);
 //        String str1 = intToEng(123);
 //        System.out.println("Exp: One Hundred Twenty Three");
 //        System.out.println("Got: " + str1);
 //        String str2 = intToEng(12345);
 //        System.out.println("Exp: Twelve Thousand Three Hundred Forty Five");
 //        System.out.println("Got: " + str2);
-        String str3 = intToEng(10001);
-        System.out.println("Exp: Ten Thousand One");
-        System.out.println("Got: " + str3);
+//        String str3 = intToEng(10001);
+//        System.out.println("Exp: Ten Thousand One");
+//        System.out.println("Got: " + str3);
+//        String str4 = intToEng(1234567891);
+//        System.out.println("Exp: One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One");
+//        System.out.println("Got: " + str4);
+        String str5 = intToEng(100000);
+        System.out.println("Exp: One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One");
+        System.out.println("Got: " + str5);
     }
 }
