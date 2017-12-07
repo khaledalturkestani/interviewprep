16a280a0ad645f6efccc2c9681b1749f4e7cac98
Khaled AlTurkestani <k.turkestani@gmail.com>
2017-12-06 19:53:37 -0800

    Change internal class name to reuse in LeetCode implementation

--- src/main/java/com/khaled/Merge2SortedLinkedLists.java
+++ src/main/java/com/khaled/Merge2SortedLinkedLists.java
@@ -5,17 +5,17 @@ package com.khaled;
  */
 public class Merge2SortedLinkedLists {
 
-    public static class Node {
+    public static class ListNode {
         int val;
-        Node next;
-        public Node(int val) {
+        ListNode next;
+        public ListNode(int val) {
             this.val = val;
         }
     }
 
-    public static Node merge(Node n1, Node n2) {
-        Node s1 = n1;
-        Node s2 = n2;
+    public static ListNode merge(ListNode n1, ListNode n2) {
+        ListNode s1 = n1;
+        ListNode s2 = n2;
         if (s1.val > s2.val) {
             s1 = n2;
             s2 = n1;
@@ -23,8 +23,8 @@ public class Merge2SortedLinkedLists {
 
         while (s1.next != null) {
             while (s2 != null && s2.val <= s1.next.val) {
-                Node temp1 = s1.next;
-                Node temp2 = s2.next;
+                ListNode temp1 = s1.next;
+                ListNode temp2 = s2.next;
                 s1.next = s2;
                 s2.next = temp1;
                 s1 = s2;
@@ -38,7 +38,7 @@ public class Merge2SortedLinkedLists {
         return (n1.val > n2.val) ? n2 : n1;
     }
 
-    public static void printList(Node n) {
+    public static void printList(ListNode n) {
         while (n != null) {
             System.out.print(n.val + ", ");
             n = n.next;
@@ -48,23 +48,23 @@ public class Merge2SortedLinkedLists {
 
     public static void main(String[] args) {
         // 1->2->3->6->7
-        Node n1 = new Node(1);
-        Node n2 = new Node(2);
-        Node n3 = new Node(3);
-        Node n4 = new Node(6);
-        Node n5 = new Node(7);
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(6);
+        ListNode n5 = new ListNode(7);
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
         n4.next = n5;
         // 1->1->2->2->4->5->8
-        Node m1 = new Node(1);
-        Node m2 = new Node(1);
-        Node m3 = new Node(2);
-        Node m4 = new Node(2);
-        Node m5 = new Node(4);
-        Node m6 = new Node(5);
-        Node m7 = new Node(8);
+        ListNode m1 = new ListNode(1);
+        ListNode m2 = new ListNode(1);
+        ListNode m3 = new ListNode(2);
+        ListNode m4 = new ListNode(2);
+        ListNode m5 = new ListNode(4);
+        ListNode m6 = new ListNode(5);
+        ListNode m7 = new ListNode(8);
         m1.next = m2;
         m2.next = m3;
         m3.next = m4;
@@ -72,7 +72,7 @@ public class Merge2SortedLinkedLists {
         m5.next = m6;
         m6.next = m7;
 
-        Node merged = merge(n1, m1);
+        ListNode merged = merge(n1, m1);
         printList(merged);
     }
 }
